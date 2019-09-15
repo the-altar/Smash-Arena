b031685d88094ac2483cd6e2dd9083c79d472744
testuser <joaovictor.clementino@gmail.com>
2019-09-15 15:51:51 -0300

    remove placeholder and test files

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"smash/engine"
 	"smash/server"
 )
 
 func main() {
-	var game engine.Game
-	m := [3]string{"Sasuke", "Sakura", "Naruto"}
-	game.Begin("Joao", m)
-
 	server.InitServer()
 }
