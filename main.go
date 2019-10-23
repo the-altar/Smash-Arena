ff04e206476e3542d2112435581041bd045fe7bd
testuser <joaovictor.clementino@gmail.com>
2019-10-22 22:52:29 -0300

    add templates

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/the-altar/Smash-Arena/providers"
 )
@@ -8,7 +10,20 @@ import (
 func main() {
 
 	g := gin.New()
+	g.LoadHTMLGlob("./templates/*")
 	g.Static("/public", "./public")
+	g.StaticFile("/arena", "public/index.html")
+
+	g.GET("/", func(g *gin.Context) {
+		g.HTML(http.StatusOK, "home.html", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	g.GET("/register", func(g *gin.Context) {
+		g.HTML(http.StatusOK, "register.html", nil)
+	})
+
 	g.GET("ws/:id", func(g *gin.Context) {
 		v := make(chan bool)
 		go providers.Conn.Init(g, v)
