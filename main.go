a1c5c66148369dde23ce8d9c0efc880fe8c7a1f7
testuser <joaovictor.clementino@gmail.com>
2019-10-24 22:27:34 -0300

    template partials add

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,24 +11,32 @@ import (
 func main() {
 
 	g := gin.New()
-	g.LoadHTMLGlob("./templates/*")
+	g.LoadHTMLGlob("templates/**/*")
 	g.Static("/public", "./public")
 	g.StaticFile("/arena", "public/index.html")
 
 	g.GET("/", func(g *gin.Context) {
-		g.HTML(http.StatusOK, "home.html", gin.H{
+		g.HTML(http.StatusOK, "home.gohtml", gin.H{
 			"title": "Main website",
 		})
 	})
 
 	g.GET("/register", func(g *gin.Context) {
-		g.HTML(http.StatusOK, "register.html", nil)
+		g.HTML(http.StatusOK, "register.gohtml", nil)
 	})
 
-	g.GET("ws/:id", func(g *gin.Context) {
+	g.GET("/ws/:id", func(g *gin.Context) {
 		v := make(chan bool)
 		go providers.Conn.Init(g, v)
 		providers.Conn.PumpOut(g.Param("id"), <-v)
+	})
+
+	g.POST("/user/new", func(g *gin.Context) {
+
+		fmt.Println(g.PostForm("username"))
+
+		g.Redirect(http.StatusMovedPermanently, "/")
+
 	})
 
 	g.Run()
